307195d1a0d10706df584f3e33526269a1d4dd7c
Cesar Hernandez <cesarihernandez92@gmail.com>
2023-06-13 10:02:03 -0700

    MERN pt2.1 update

--- backend/api/reviews.controller.js
+++ backend/api/reviews.controller.js
@@ -1,1 +1,16 @@
 /* This will handle data requests for reviews.*/
+
+export default class ReviewsController {
+
+    static async apiPostReview(req, res, next) {
+        // TODO:
+    }
+
+    static async apiUpdateReview(req, res, next) {
+        // TODO:
+    }
+
+    static async apiDeleteReview(req, res, next) {
+        // TODO:
+    }
+}
--- backend/dao/moviesDAO.js
+++ backend/dao/moviesDAO.js
@@ -26,5 +26,58 @@ export default class MoviesDAO {
         moviesPerPage = 20,
     } = {}) { // empty object as default value
         let query;
+        if (filters) {
+            if ('title' in filters) {
+                query = { $text: { $search: filters['title'] } };
+            } else if ('rated' in filters) {
+                query = { 'rated': {$eq: filters['rated'] } };
+            }
+        }
+
+        let cursor;
+        try {
+            cursor = await movies.find(query)
+                                 .limit(moviesPerPage)
+                                 .skip(moviesPerPage * page);
+            const moviesList = await cursor.toArray();
+            const totalNumMovies = await movies.countDocuments(query);
+            return { moviesList, totalNumMovies};
+        }   catch (e) {
+            console.error(`Unable to issue find commamnd, ${e}`);
+            return { moviesList: [], totalNumMovies: 0 };
+        }
+    }
+
+    static async getRatings() {
+        let ratings = [];
+        try {
+            ratings = await movies.distinct('rated');
+            return ratings;
+        } catch (e) {
+            console.error(`Unable to get ratings, ${e}`);
+            return ratings;
+        }
+    }
+
+    static async getMovieById(id) {
+        try {
+            return await movies.aggregate([
+                {
+                    $match: {
+                        _id: new ObjectId(id),
+                    }
+                },
+                {
+                    $lookup: {
+                        from: 'reviews',
+                        localField: '_id',
+                        foreignField: 'movie_id',
+                        as: 'reviews',
+                    }
+                }
+            ]).next();
+        } catch (e) {
+            console.error(`Unable to get movie by ID: ${e}`);
+        }
     }
 }
