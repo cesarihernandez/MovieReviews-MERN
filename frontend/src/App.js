fb6292d75322606910269de456445493712d9b00
Cesar Hernandez <cesarihernandez92@gmail.com>
2023-06-19 12:28:26 -0700

    MERN pt3

---
+++ frontend/src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import {Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import { Navbar, Nav } from "react-bootstrap";
+
+import MoviesList from "./components/MoviesList";
+import Movie from "./components/Movie";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="primary" expand="lg" sticky="top" variant="dark">
+        <Container className="container-fluid">
+          <Navbar.Brand href="/">
+            <img src="/images/movies-logo.png" alt="movies logo" className="moviesLogo"/>
+          MOVIE TIME
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-atuo">
+              <Nav.Link as={Link} to="/movies">
+                Movies
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Routes>
+        <Route exact path="/" element={
+          <MoviesList />}
+        />
+        <Route exact path="/movies" element={
+          <MoviesList />}
+        />
+        <Route path="/movies/:id" element={
+          <Movie />}
+        />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/MoviesList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MoviesList = props => {
+    return (
+        <div className="App">
+            Placeholder text for MoviesList
+        </div>
+    )
+}
+
+
+export default MoviesList;
